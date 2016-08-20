0ee9cf5990cf37593a17082b2e3148414959a62a
Scott Holland <scotthollandjr@gmail.com>
2016-08-20 12:55:47 -0700

    Update to include react and es2015 babel presets

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res) {
-  res.send("<h1>Test Title</h1><h3>Boots and Cats</h3>");
+  res.render('index', {});
 });
 
 module.exports = router;
