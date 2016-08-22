fb32dd35df9bdaf6868bc95de5ca28a567871850
Scott Holland <scotthollandjr@gmail.com>
2016-08-22 11:53:59 -0700

    Add broken profile modal

--- views/about.jsx
+++ views/about.jsx
@@ -1,28 +1,36 @@
 var React = require('react');
+var Modal = require('react-modal');
 var DefaultLayout = require('./layout');
 
 var AboutComponent = React.createClass({
+
+  openModal: function() {
+    this.setState({className: is-active});
+  },
+
+  closeModal: function() {
+    this.setState({modalIsOpen: false});
+  },
+
   render: function() {
     return (
       <DefaultLayout name={this.props.name}>
 
+        <a onClick={this.openModal} className="button is-primary">modal</a>
+        <div className="modal" id="madmodal">
+          <div className="modal-background"></div>
+          <div className="modal-card">
+            <header className="modal-card-head">
+              <p className="modal-card-title">Modal title</p>
+              <button className="delete"></button>
+            </header>
+            <section className="modal-card-body">
 
-      <a className="button is-primary is-large modal-button">button</a>
-          <div className="modal">
-            <div className="modal-background"></div>
-            <div className="modal-card">
-              <header className="modal-card-head">
-                <p className="modal-card-title">Modal title</p>
-                <button className="delete"></button>
-              </header>
-              <section className="modal-card-body">
-
-              </section>
-              <footer className="modal-card-foot">
-                <a className="button is-primary">Save changes</a>
-                <a className="button">Cancel</a>
-              </footer>
-            </div>
+            </section>
+            <footer className="modal-card-foot">
+              <a className="button is-primary">Save changes</a>
+              <a onClick={this.closeModal} className="button">Cancel</a>
+            </footer>
           </div>
         </div>
 
